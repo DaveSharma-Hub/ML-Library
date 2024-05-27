2a846fe13609572b95b0b4b5960a69af5957ae88
DaveSharma-Hub <davesharma12345@outlook.com>
2024-05-26 22:43:22 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 using namespace operations;
 using namespace std;
 
-void test(){
+void test(double* sum1, double* sum2, double* sum3){
     vector<vector<int>> a = createMatrix(30,1500000);
     vector<vector<int>> b = createMatrix(1500000,20);
     vector<vector<int>> transposedB = transpose(b);
@@ -34,15 +34,27 @@ void test(){
     double elapsed_time_ms = std::chrono::duration<double, std::milli>(t_end-t_start).count();
     double elapsed_time2_ms = std::chrono::duration<double, std::milli>(t_end2-t_start2).count();
     double elapsed_time3_ms = std::chrono::duration<double, std::milli>(t_end3-t_start3).count();
-    cout<<"Naive:"<<elapsed_time_ms<<endl;
-    cout<<"Transposed Naive:"<<elapsed_time2_ms<<endl;
-    cout<<"Wrong Mult:"<<elapsed_time3_ms<<endl;
-    cout<<"COMPLETED"<<endl;
+    // cout<<"Naive:"<<elapsed_time_ms<<endl;
+    // cout<<"Transposed Naive:"<<elapsed_time2_ms<<endl;
+    // cout<<"Wrong Mult:"<<elapsed_time3_ms<<endl;
+    // cout<<"COMPLETED"<<endl;
+    *sum1 +=  elapsed_time_ms;
+    *sum2 +=  elapsed_time2_ms;
+    *sum3 +=  elapsed_time3_ms;
 }
 
 
 int main() {
     srand(time(0)); 
-    test();
+    double* first = new double(0);
+    double* second = new double(0);
+    double* third = new double(0);
+    int count = 50;
+    for(int i=0;i<count;i++){
+        test(first, second, third);
+    }
+    cout<<"Naive:"<<*first/count<<endl;
+    cout<<"Transposed:"<<*second/count<<endl;
+    cout<<"Wrong:"<<*third/count<<endl;
     return 0;
 }
